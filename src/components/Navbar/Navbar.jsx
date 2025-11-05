2ba0cf1ec4a0b5b1c959284c987f735651c60981
reham <rehamshipl445@gmail.com>
2025-11-05 03:57:59 +0200

    Comment out SVG in Navbar component
    Commented out the SVG element in the Navbar component.

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -29,7 +29,7 @@ function Navbar() {
     <div className="navbar-start">
       <div className="dropdown">
         <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-          <svg
+          {/* <svg
             xmlns="http://www.w3.org/2000/svg"
             className="h-5 w-5"
             fill="none"
@@ -40,7 +40,8 @@ function Navbar() {
               strokeLinejoin="round"
               strokeWidth="2"
               d="M4 6h16M4 12h8m-8 6h16" />
-          </svg>
+          </svg> */}
+          <h1 className=" text-xl line-tight"> Cliniqly</h1>
         </div>
         <ul
           tabIndex={0}
